e65e472094331197b29f16ff90e216729211b145
MD-ABDULLAH-FARDUS <abdullahferdus@gmail.com>
2021-04-09 21:40:13 +0600

    commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Css from '../node_modules/bootstrap/dist/css/bootstrap'
+import Css from '../node_modules/bootstrap/dist/css/bootstrap.css'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(<App />,document.getElementById('root'));
 
